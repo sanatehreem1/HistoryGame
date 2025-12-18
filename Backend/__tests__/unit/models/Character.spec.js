cf5704124239f2acc22155581b56020b32dc0332
najhdr <najwahaifahedar@gmail.com>
2025-12-18 11:25:48 +0000

    character model testing

--- Backend/__tests__/unit/models/Character.spec.js
+++ Backend/__tests__/unit/models/Character.spec.js
@@ -1,93 +1,124 @@
-const Character = require('../../../models/Character')
-const db = require('../../../database/connection')
+const Character = require("../../../models/Character");
+const db = require("../../../database/connection");
 
-describe('Character', () => {
-    beforeEach(() => jest.clearAllMocks())
-    
-    afterAll(() => jest.resetAllMocks())
-})
+describe("Character", () => {
+  beforeEach(() => jest.clearAllMocks());
 
-describe('constructor', () => {
-    it('creates a Character instance from database', () => {
-        const row = {
-            character_id: 1,
-            name: 'Ghost',
-            early_life: 'Unknown',
-            marriage:'None',
-            fun_fact:'blabla',
-            death: 'mysterious',
-            haunting_motives: 'revenge',
-            correct_answer: true,
-            image_url: 'wife.png',
-            story_id: 2
+  afterAll(() => jest.resetAllMocks());
 
-        }
+  describe("constructor", () => {
+    it("creates a Character instance from database", async () => {
+      const row = {
+        character_id: 1,
+        name: "Ghost",
+        early_life: "Unknown",
+        marriage: "None",
+        fun_fact: "blabla",
+        death: "mysterious",
+        haunting_motives: "revenge",
+        correct_answer: true,
+        image_url: "wife.png",
+        story_id: 2,
+      };
 
-        const character = new Character(row)
+      const character = await new Character(row);
 
-        expect(character).toBeInstanceOf(Character)
-        expect(character.character_id).toBe(1)
-        expect(character.name).toBe('Ghost')
-        expect(character.haunting_motives).toBe('revenge')
-        expect(character.story_id).toBe('2')
+      expect(character).toBeInstanceOf(Character);
+      expect(character.character_id).toBe(1);
+      expect(character.name).toBe("Ghost");
+      expect(character.haunting_motives).toBe("revenge");
+      expect(character.story_id).toBe(2);
+    });
+  });
+
+  describe("getAll", () => {
+    it("returns an array of Character", async () => {
+      const mockRows = [
+        {
+          character_id: 1,
+          name: "Ghost A",
+          early_life: "hmm",
+          marriage: "ok",
+          fun_fact: "fun",
+          death: "unknown",
+          haunting_motives: "fear",
+          correct_answer: false,
+          image_url: "b.png",
+          story_id: 1,
+        },
+      ];
+
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: mockRows });
+
+      const result = await Character.getAll();
+      expect(db.query).toHaveBeenCalledTimes(1);
+      expect(result.length).toBe(1);
+      expect(result[0]).toBeInstanceOf(Character);
+      expect(result[0].name).toBe("Ghost A");
+    });
+  });
+
+  describe("getOneByID", () => {
+    it("returns a Character when found", async () => {
+      const mockRow = {
+        character_id: 2,
+        name: "Ghost B",
+        early_life: "zzz",
+        marriage: null,
+        fun_fact: "creepy",
+        death: "idk",
+        haunting_motives: "justice",
+        correct_answer: false,
+        image_url: "c.png",
+        story_id: 3,
+      };
+
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [mockRow] });
+      const result2 = await Character.getOneByID(2);
+      expect(result2).toBeInstanceOf(Character);
+      expect(result2.name).toBe("Ghost B");
+    });
+
+    it("throwns an error when character is not found", async () => {
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
+
+      await expect( Character.getOneByID(999)).rejects.toThrow("Unable to locate character!!")
+     
+    });
+  });
+
+  describe("getAllByStory", () => {
+    it("return all characters of a story when found", async() => {
+       const mockRow = {
+        story_id: 2,
+        name: "Ghost B",
+        early_life: "zzz",
+        marriage: null,
+        fun_fact: "creepy",
+        death: "idk",
+        haunting_motives: "justice",
+        correct_answer: false,
+        image_url: "c.png",
+        story_id: 3,
+      }; 
+
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [mockRow] });
+        const result3 = await Character.getAllByStory(3);
+      expect(result3).toBeInstanceOf(Array);
+      expect(result3[0].name).toBe("Ghost B");
+    });
+
+    it("throwns an error when character is not found", async () => {
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
+
+      await expect( Character.getOneByID(999)).rejects.toThrow("Unable to locate character!!")
+     
+    });
+
+
     })
-})
 
-describe('getAll', () => {
-    it('retuns an array of Character', async () => {
-        const mockRows = [
-            {
-            character_id: 1,
-            name: 'Ghost A',
-            early_life: 'hmm',
-            marriage:'ok',
-            fun_fact:'fun',
-            death: 'unknown',
-            haunting_motives: 'fear',
-            correct_answer: false,
-            image_url: 'b.png',
-            story_id: 1
-            }
-        ]
 
-        jest.spyOn(db, 'query').mockResolvedValueOnce({rows: mockRows})
-
-        const result = await Character.getAll()
-        expect(db.query).toHaveBeenCalledTimes(1)
-        expect(result.length).toBe(1)
-        expect(character.name).toBe('Ghost')
-        expect(result[0]).toBeInstanceOf(Character)
-        expect(result[0].name).toBe('Ghost A')
-
-    })
-})
-
-describe('getOneByID', () => {
-    it('returns a Character when found', async () => {
-        const mockRow = {   
-            character_id: 2,
-            name: 'Ghost B',
-            early_life: 'zzz',
-            marriage: null,
-            fun_fact:'creepy',
-            death: 'idk',
-            haunting_motives: 'justice',
-            correct_answer: false,
-            image_url: 'c.png',
-            story_id: 3
-        }
-
-        jest.spyOn(db, 'query').mockResolvedValueOnce({ rows: [mockRow]} )
-        const result= await Character.getOneByID(2)
-        expect(result).toBeInstanceOf(Character)
-        expect(result.name).toBe('Ghost B')
-    })
-    
-    it('throwns an error when character is not found', async () => {
-        jest.spyOn(db,'query').mockResolvedValueOnce({rows: []})
-
-        await expect(Character.getOneByID(999).rejects.toThrow('Unable to locate character'))
-    })
-})
+});
 
 
